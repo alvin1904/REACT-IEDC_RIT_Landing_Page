d752a35a5c4b805ca698cc1e9f10a834763c1046
alvin1904 <alvin19official@gmail.com>
2023-02-11 08:25:45 +0530

    feat: add glass effect

--- src/components/Main/Landing_main.jsx
+++ src/components/Main/Landing_main.jsx
@@ -1,10 +1,10 @@
 const Landing_main = () => {
   return (
-    <>
+    <div className="main_main">
       <div className="header">
         INNOVATION AND ENTREPRENEURSHIP DEVELOPMENT CENTRE
       </div>
-    </>
+    </div>
   );
 };
 export default Landing_main;
