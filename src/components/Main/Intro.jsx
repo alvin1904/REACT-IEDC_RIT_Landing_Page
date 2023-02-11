d640e40886e70f0b0f0f043d6f9d35a50f7cd66b
alvin1904 <alvin19official@gmail.com>
2023-02-11 19:17:23 +0530

    few css adjustments and tuning

--- src/components/Main/Intro.jsx
+++ src/components/Main/Intro.jsx
@@ -1,24 +1,19 @@
 const Intro = () => {
   return (
     <div className="about_header">
-      <div className="about_heading">About IEDC RIT</div>
       <div className="about_text">
+        <div className="about_heading">About IEDC RIT</div>
         Innovation and entrepreneurship cell RIT is a vibrant student-run
         organisation aiming to empower student minds and guide them throughout
-        the path of innovation.
-        <br></br>
-        IEDC RIT promote the culture of creative thinking and work toward
-        transformation of potential innovative ideas and projects into future
-        enterprises and game changer products.
-        <br></br>
-        We, at IEDC RIT, believe in nurturing all young minds out there into
-        capable leaders and potential entrepreneurs who can move forward and
-        excel in their careers and play a major roles in the advancement of the
-        world to it's better versions.
-        <br></br>
-        IEDC RIT intend to provide ample support, resources and guidance to all
-        RITians to achieve excellence and beyond through innovation and
-        entrepreneurship.
+        the path of innovation. IEDC RIT promote the culture of creative
+        thinking and work toward transformation of potential innovative ideas
+        and projects into future enterprises and game changer products. We, at
+        IEDC RIT, believe in nurturing all young minds out there into capable
+        leaders and potential entrepreneurs who can move forward and excel in
+        their careers and play a major roles in the advancement of the world to
+        it's better versions. IEDC RIT intend to provide ample support,
+        resources and guidance to all RITians to achieve excellence and beyond
+        through innovation and entrepreneurship.
       </div>
     </div>
   );
