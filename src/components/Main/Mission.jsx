abcecf399b989ed35ca0b4fc5861e1773d1ef9d1
alvin1904 <alvin19official@gmail.com>
2023-02-11 20:28:42 +0530

    css adjustments nd mission vision sections

--- src/components/Main/Mission.jsx
+++ src/components/Main/Mission.jsx
@@ -1,5 +1,31 @@
 const Mission = () => {
-    return <>Mission</>;
-  };
-  export default Mission;
-  
+  return (
+    <div className="mission_header">
+      <div className="mission_container">
+        <h1 className="mission_heading">Mission</h1>
+        <p className="mission_text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+          pellentesque venenatis risus, ut placerat odio finibus consectetur.
+          Suspendisse faucibus molestie dolor vel lobortis. Nulla facilisi. Sed
+          felis lorem, rhoncus a convallis quis, accumsan sit amet justo.
+          Pellentesque vel ex quam. Donec eu tempus erat. Duis vel elit cursus
+          diam varius molestie. Vivamus id tempor lacus, a efficitur turpis.
+          Quisque convallis ipsum ac dui congue, quis commodo magna convallis.
+        </p>
+      </div>
+      <div className="mission_container">
+        <h1 className="mission_heading">Vision</h1>
+        <p className="mission_text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+          pellentesque venenatis risus, ut placerat odio finibus consectetur.
+          Suspendisse faucibus molestie dolor vel lobortis. Nulla facilisi. Sed
+          felis lorem, rhoncus a convallis quis, accumsan sit amet justo.
+          Pellentesque vel ex quam. Donec eu tempus erat. Duis vel elit cursus
+          diam varius molestie. Vivamus id tempor lacus, a efficitur turpis.
+          Quisque convallis ipsum ac dui congue, quis commodo magna convallis.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Mission;
